e1a2dd3f3ead8b6352681f909f062ac06262f3d1
JuanRamos15 <124464419+JuanRamos15@users.noreply.github.com>
2023-02-09 13:57:33 +0000

    test de modo estricto

---
+++ test.js
@@ -0,0 +1,8 @@
+"use strict";
+let user ="Usnavi";
+console.log(user);
+
+//let es toda la variable que se pueda declarar en java si esxisten tipos de datos
+
+let nombre = "juan";
+console.log(`${nombre}`);
